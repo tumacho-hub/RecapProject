d80e9b1307ed3882544c5543da1244bd3518bf47
Jorge Flores <jorgeareszuma86@gmail.com>
2021-06-30 18:48:00 -0500

    second User story 100

--- src/test/java/UserStory100.java
+++ src/test/java/UserStory100.java
@@ -1,6 +1,7 @@
 public class UserStory100 {
     public static void main(String[] args) {
         System.out.println( "Let's create one commit, work one ");
+        System.out.println("second commit");
 
     }
 }
