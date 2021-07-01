f5b1d16bc56147387c8ff166b6d989250dd2a707
tumacho-hub <82963119+tumacho-hub@users.noreply.github.com>
2021-06-30 20:27:53 -0500

    Us102 has been completed (#2)
    Combined all 3 commit by squashing it

---
+++ src/test/java/UserStory102.java
@@ -0,0 +1,8 @@
+public class UserStory102 {
+    public static void main(String[] args) {
+        System.out.println( "I like your work");
+        System.out.println(" this will be the second commit in this branch");
+        System.out.println("Just messing around ");
+        System.out.println("ready to push");
+    }
+}
